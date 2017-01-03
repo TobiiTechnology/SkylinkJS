82b41c9ee74daac66a18061c85a14e5a5b8ad536
Erik Alsmyr <erik.alsmyr@tobii.com>
2017-01-03 09:12:38 +0100

    publish 0.6.13-tobii.1

--- publish/skylink.complete.js
+++ publish/skylink.complete.js
@@ -1,4 +1,4 @@
-/*! skylinkjs - v0.6.13 - Thu Aug 18 2016 10:56:34 GMT+0200 (W. Europe Daylight Time) */
+/*! skylinkjs - v0.6.13 - Tue Jan 03 2017 08:55:55 GMT+0100 (CET) */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.io = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 
@@ -10278,7 +10278,7 @@ if ( navigator.mozGetUserMedia ||
             clearInterval(checkIfReady);
 
             baseGetUserMedia(updatedConstraints, successCb, function (error) {
-              if (['PermissionDeniedError', 'SecurityError'].indexOf(error.name) > -1 && window.parent.location.protocol === 'https:') {
+              if (['PermissionDeniedError', 'SecurityError', 'NotAllowedError'].indexOf(error.name) > -1 && window.parent.location.protocol === 'https:') {
                 AdapterJS.renderNotificationBar(AdapterJS.TEXT.EXTENSION.REQUIRE_INSTALLATION_FF,
                   AdapterJS.TEXT.EXTENSION.BUTTON_FF,
                   'https://addons.mozilla.org/en-US/firefox/addon/tobii-pro-screensharing/', true, true);
@@ -10474,7 +10474,7 @@ if ( navigator.mozGetUserMedia ||
   }
 })();
 
-/*! skylinkjs - v0.6.13 - Thu Aug 18 2016 10:56:34 GMT+0200 (W. Europe Daylight Time) */
+/*! skylinkjs - v0.6.13 - Tue Jan 03 2017 08:55:55 GMT+0100 (CET) */
 
 (function() {
 
